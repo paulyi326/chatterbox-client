1c25116233863ea306d8c7d0cb2748db398223e8
paulyi326 <paul.yi326@gmail.com>
2014-06-16 14:27:47 -0700

    implement app.send function

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,24 @@
 // YOUR CODE HERE:
+//
+var app = {
+  init: function(){
+    //init code here
+  },
+  send: function(message){
+    //send code here
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'POST',
+      data: JSON.stringify(message),
+      success: function(data){
+        console.log('chatterbox: Message sent');
+      },
+      error: function(data){
+        console.error('chatterbox: Failed to send message');      }
+    });
+  },
+  fetch: function(){
+    //fetch code here
+  }
+};
 
