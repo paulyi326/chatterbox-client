3be431d0aa4f98f32e64f79fe1f16a7a2677439d
paulyi326 <paul.yi326@gmail.com>
2014-06-16 15:41:26 -0700

    implemented addRoom

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,28 +1,42 @@
 // YOUR CODE HERE:
 //
 var app = {
-  server: 'https://api.parse.com/1/classes/chatterbox',
+  // server: 'https://api.parse.com/1/classes/chatterbox',
 
   init: function(){
     //init code here
+    this.server = 'https://api.parse.com/1/classes/chatterbox';
+    this.currentRoom = 1;
+    this.allocatedRooms = 1;
+    // var gotInput = false;
+    // while(!gotInput){
+    //   var name = prompt('Enter name');
+    //   if( confirm('Confirm your name is ' + name) ){
+    //     gotInput = true;
+    //   }
+    // }
+    // this.username = name;
   },
+
   send: function(message){
     //send code here
     $.ajax({
-      url: app.server,
+      url: this.server,
       type: 'POST',
       data: JSON.stringify(message),
       success: function(data){
         console.log('chatterbox: Message sent');
       },
       error: function(data){
-        console.error('chatterbox: Failed to send message');      }
+        console.error('chatterbox: Failed to send message');
+      }
     });
   },
+
   fetch: function(callback){
     //fetch code here
     $.ajax({
-      url: app.server,
+      url: this.server,
       type: "GET",
       dataType: 'json',
       success: function(data){
@@ -32,6 +46,20 @@ var app = {
         console.log(xhr.statusText);
       }
     });
+  },
+
+  clearMessages: function() {
+    $('#chats').empty();
+  },
+
+  addMessage: function(message) {
+    var newMessage  = $('<li>' + message.name + ': ' + message.text + '</li>');
+    $('#chats').append(newMessage);
+  },
+
+  addRoom: function(roomName) {
+    var newRoom = $('<button type="button">' + roomName + '</button>');
+    $('#roomSelect').append(newRoom);
   }
 };
 
